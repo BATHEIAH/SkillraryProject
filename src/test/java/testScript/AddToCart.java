cb0075b278717c341eed6b5ccbd0a17f05528d0f
Batheiah <Batheiah@Batheiah143>
2023-05-26 10:06:07 +0530

    testng.xml added and pom.xml updated

--- src/test/java/testScript/AddToCart.java
+++ src/test/java/testScript/AddToCart.java
@@ -5,32 +5,32 @@ import org.testng.asserts.SoftAssert;
 
 import Genericlibraries.BaseClass;
 
-public class AddToCart extends BaseClass
-{
-@Test
-public void addtoCart() {
-	SoftAssert soft=new SoftAssert();
-	
-	home.ClickGearsTab();
-	home.ClickSkillraryDemoApp();
-	web.handleChildBrowser();
-	
-	soft.assertTrue(demoApp.getlogo().contains("ECommerce"));
-	demoApp.mouseoverToCourse(web);
-	demoApp.ClickSeleniumTraining();
-	
-	soft.assertEquals(selenium.getpageheader(), "selenium Training");
-	
-	int initialQuantity=Integer.parseInt(selenium.getquantity());
-	selenium.DoubleClickPlusButton(web);
-	int finalQuantity=Integer.parseInt(selenium.getquantity());
-	soft.assertEquals(finalQuantity, initialQuantity);
-	
-	selenium.clickAddToCart();
-	web.handleAlert("ok");
-	soft.assertEquals(selenium.getmassage(),"Item added to cart");
-	
-	soft.assertAll();
-			
-}
+public class AddToCart extends BaseClass {
+	@Test
+	public void addtoCart()
+	{
+		SoftAssert soft = new SoftAssert();
+
+		home.ClickGearsTab();
+		home.ClickSkillraryDemoApp();
+		web.handleChildBrowser();
+
+		soft.assertTrue(demoApp.getlogo().contains("ECommerce"));
+		demoApp.mouseoverToCourse(web);
+		demoApp.ClickSeleniumTraining();
+
+		soft.assertEquals(selenium.getpageheader(), "selenium Training");
+
+		int initialQuantity = Integer.parseInt(selenium.getquantity());
+		selenium.DoubleClickPlusButton(web);
+		int finalQuantity = Integer.parseInt(selenium.getquantity());
+		soft.assertEquals(finalQuantity, initialQuantity);
+
+		selenium.clickAddToCart();
+		web.handleAlert("ok");
+		soft.assertEquals(selenium.getmassage(), "Item added to cart");
+
+		soft.assertAll();
+
+	}
 }
